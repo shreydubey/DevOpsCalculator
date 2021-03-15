01f86da28c9db77da4c59934d6081caa35d136d4
shrey <shreydubey15@gmail.com>
2021-03-15 15:21:08 +0530

    Completion Commit 4

--- src/test/java/ScientificCalculator/DevOpsCalculatorTest.java
+++ src/test/java/ScientificCalculator/DevOpsCalculatorTest.java
@@ -14,7 +14,8 @@ public class DevOpsCalculatorTest {
     }
 
     @Test
-    public void testSquareRoot() {
+    public void testSquareRootTruePositive() {
+        System.out.print("\n\nTesting Square Root For True Positive");
         double a = 25.0;
         double expectedResult = 5.0;
         double result = calculator.SquareRoot(a);
@@ -22,7 +23,17 @@ public class DevOpsCalculatorTest {
     }
 
     @Test
-    public void testFactorial() {
+    public void testSquareRootFalsePositive() {
+        System.out.print("\n\nTesting Square Root For False Positive");
+        double a = 25.0;
+        double expectedResult = 6.0;
+        double result = calculator.SquareRoot(a);
+        Assert.assertNotEquals(expectedResult, result, 0);
+    }
+
+    @Test
+    public void testFactorialTruePositive() {
+        System.out.print("\n\nTesting Factorial For True Positive");
         double a = 4.0;
         double expectedResult = 24.0;
         double result = calculator.Factorial(a);
@@ -30,7 +41,18 @@ public class DevOpsCalculatorTest {
     }
 
     @Test
-    public void testNaturalLog() {
+    public void testFactorialFalsePositive() {
+        System.out.print("\n\nTesting Factorial For False Positive");
+        double a = 4.0;
+        double expectedResult = 23.0;
+        double result = calculator.Factorial(a);
+        Assert.assertNotEquals("hello",expectedResult, result, 0);
+    }
+
+
+    @Test
+    public void testNaturalLogTruePositive() {
+        System.out.print("\n\nTesting Natural Log For True Positive");
         double a = 1.0;
         double expectedResult = 0.0;
         double result = calculator.NaturalLog(a);
@@ -38,12 +60,34 @@ public class DevOpsCalculatorTest {
     }
 
     @Test
-    public void testPowerFunction() {
+    public void testNaturalLogFalsePositive() {
+        System.out.print("\n\nTesting Natural Log For False Positive");
+        double a = 1.0;
+        double expectedResult = 10.0;
+        double result = calculator.NaturalLog(a);
+        Assert.assertNotEquals(expectedResult, result, 0);
+    }
+
+    @Test
+    public void testPowerFunctionTruePositive() {
+        System.out.print("\n\nTesting Power Function For True Positive");
         double a = 2.0;
         double b = 4.0;
         double expectedResult = 16.0;
         double result = calculator.Power(a,b);
         Assert.assertEquals(expectedResult, result, 0.0001);
     }
+
+    @Test
+    public void testPowerFunctionFalsePositive() {
+        System.out.print("\n\nTesting Power Function For False Positive\n\n");
+        double a = 2.0;
+        double b = 4.0;
+        double expectedResult = 32.0;
+        double result = calculator.Power(a,b);
+        Assert.assertNotEquals(expectedResult, result, 0.0001);
+    }
+
+
 
 }
